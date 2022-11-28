023d5fadacfa728d75c7c6c8ec6932d0c98d4b8c
Marek Mudron <mudronmarek@gmail.com>
2022-11-28 11:38:39 +0100

    fix script

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #/usr/bin/bash
-python3 get_urls.py > product_urls.txt
-python3 get_price.py < product_urls.txt
+python3 get_urls.py > urls.txt
+
+head -20 urls.txt | 
+while read URL; do
+  python3 get_price.py URL; 
+done
